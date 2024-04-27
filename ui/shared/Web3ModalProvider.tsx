943ba54b7ef229c2d8c08d28d135a3d02bfd18f5
大海 <iwangyang@qq.com>
2024-04-27 23:03:06 +0800

    Update Web3ModalProvider.tsx

--- ui/shared/Web3ModalProvider.tsx
+++ ui/shared/Web3ModalProvider.tsx
@@ -1,8 +1,8 @@
 import { useColorMode } from '@chakra-ui/react';
-// import { PolisProvider } from '@metis.io/middleware-client';
+import { PolisProvider } from '@metis.io/middleware-client';
 import { createWeb3Modal, useWeb3ModalTheme } from '@web3modal/wagmi/react';
 import axios from 'axios';
-// import { ethers } from 'ethers';
+import { ethers } from 'ethers';
 import React from 'react';
 import { WagmiProvider } from 'wagmi';
 
@@ -17,7 +17,7 @@ const NUVO_DAPP_ID = '64bf8264ccdabc001392582f'; // admin testnet app id
 const NUVO_DAPP_KEY = 'd90da492d517476d8d47f49e6a6c46b6'; // admin testnet app key
 // const NUVO_OAUTH = 'https://oauth.staging.nuvosphere.io';
 const NUVO_API = 'https://api.staging.nuvosphere.io';
-// const NUVO_CHAIN_ID = 59902;
+const NUVO_CHAIN_ID = 59902;
 
 const initProvider = () => {
   const currentUrl = new URL(window.location.href);
@@ -48,15 +48,15 @@ const initProvider = () => {
       params: { access_token: accessToken },
     }).then(({ data: res }) => {
       if (res.code === 200) {
-        // const address = res.data.eth_address;
-        // // get provider
-        // const polisProvider = new PolisProvider({
-        //   apiHost: NUVO_API,
-        //   token: accessToken,
-        //   chainId: NUVO_CHAIN_ID,
-        // });
-        // const provider = new ethers.providers.Web3Provider(polisProvider);
-        // console.log('🌊', provider, address);
+        const address = res.data.eth_address;
+        // get provider
+        const polisProvider = new PolisProvider({
+          apiHost: NUVO_API,
+          token: accessToken,
+          chainId: NUVO_CHAIN_ID,
+        });
+        const provider = new ethers.providers.Web3Provider(polisProvider);
+        console.log('🌊', provider, address);
       } else {
         localStorage.removeItem('AccessToken');
         // Access token not existed
